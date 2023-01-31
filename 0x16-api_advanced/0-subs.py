16dede466aa4ecc25238c34d0b34eb644396749d
atom-data <alatatomondi@gmail.com>
2023-01-31 12:20:37 +0000

    query git api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""0-subs.py - Query Reddit API & find # subscribers"""
+
+import json
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+
+    """Return the number of subscribers"""
+
+    baseURL = "https://www.reddit.com/r/"
+
+    url = baseURL + subreddit + "/about/.json"
+
+    headers = {
+
+        'User-Agent': 'My User Agent 1.0',
+        'From': '214@holbertonschool.com'
+
+    }
+
+    result = requests.get(url, headers=headers)
+
+    if result.status_code == 200:
+
+        result = result.json()
+
+        return result['data']['subscribers']
+
+    else:
+
+        return 0
