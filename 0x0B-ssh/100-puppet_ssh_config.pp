50c5ca94e467432e438a0d75c609f0abf9719175
atom-data <alatatomondi@gmail.com>
2023-03-26 05:45:07 -0700

    server configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,8 @@
-#changes SSH config file
-exec { '/root/.ssh/config':
-  command => '/usr/bin/cat ./2-ssh_config > /root/.ssh/config',
+#change ssh client configurations
+$contents = "Host *\n\tPasswordAuthentication yes\n\nHost 54449-web-01\n\tHostname 100.26.244.193\n\tUser ubuntu\n\tPubKeyAuthentication yes\n\tPreferredAuthentications publickey\n\tIdentityFile ~/.ssh/school\n\tIdentitiesOnly yes\n\tPasswordAuthentication no"
+file { '/root/.ssh/config':
+ ensure  => 'file',
+ path    => '/root/.ssh/config',
+ mode    => '0644',
+ content => $contents
 }
